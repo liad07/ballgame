ad639e1cd2c52eaef28fd97bd56141625e4cc52f
liadk07 <91607695+liad07@users.noreply.github.com>
2022-01-15 21:02:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function myFunction() {
     var user = document.getElementById("user").value;
     var password = document.getElementById("password").value;
     // document.getElementById("demo").innerHTML = x;
-    if (user=="dvirk07"&&password=="li230878"||user=="liad07"&&password=="li230878"){
+    if (user=="admin"&&password=="4321"){
         document.writeln("its your ")
         let text = "link";
         let result = text.link( "iptv.html");

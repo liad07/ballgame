82fe2320f552b3d324d4f9ff7a937c654836e979
liadk07 <91607695+liad07@users.noreply.github.com>
2022-01-15 21:01:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var user = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+    // document.getElementById("demo").innerHTML = x;
+    if (user=="dvirk07"&&password=="li230878"||user=="liad07"&&password=="li230878"){
+        document.writeln("its your ")
+        let text = "link";
+        let result = text.link( "iptv.html");
+        document.writeln(result)
+
+    }
+else {
+    document.writeln("wrong password or user")
+    }
+}
